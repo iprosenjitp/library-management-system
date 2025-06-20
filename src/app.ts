1b07b530e2e006cdeb2b25602c326ddb8a07caa2
Prosenjit Pal <iprosenjitp@yahoo.com>
2025-06-20 19:50:42 +0600

    Add borrow routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application, NextFunction, Request, Response } from 'express';
 import { bookRoutes } from './app/controllers/book.controller';
+import { borrowRoutes } from './app/controllers/borrow.controller';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use('/books', bookRoutes);
+app.use('/borrow', borrowRoutes);
 
 app.get('/', async (req: Request, res: Response) => {
     res.send("Welcome to Library Management System");
