b97249c624fa2ca2bc9e92dc2d36048a1115a475
Prosenjit Pal <iprosenjitp@yahoo.com>
2025-06-20 17:46:51 +0600

    Update app file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Application, NextFunction, Request, Response } from 'express';
+import { bookRouters } from './app/controllers/book.controller';
 
 const app: Application = express();
+
+app.use(express.json());
+app.use('/books', bookRouters);
 
 app.get('/', async (req: Request, res: Response) => {
     res.send("Welcome to Library Management System");
