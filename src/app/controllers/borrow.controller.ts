b13cfde93aaa8e54ecd51dac16bc7b35f3afdd25
Prosenjit Pal <iprosenjitp@yahoo.com>
2025-06-20 18:51:47 +0600

    Create borrow routes

---
+++ src/app/controllers/borrow.controller.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from 'express';
+import { Borrow } from '../models/borrow.model';
+
+export const borrowRoutes = express.Router();
+
+// POST - Create borrow
+borrowRoutes.post('/', async (req: Request, res: Response) => {
+    try {
+        const body = req.body;
+        const borrow = await Borrow.create(body);
+
+        res.status(201).json({
+            success: true,
+            message: "Book borrowed successfully",
+            data: borrow
+        });
+    } catch (error: any) {
+        res.status(400).json({
+            success: false,
+            message: "Validation failed",
+            error: error
+        });
+    }
+});
+
+// GET - Retrieve all borrows
+borrowRoutes.get('/', async (req: Request, res: Response) => {
+    const books = await Borrow.find();
+    
+    res.status(201).json({
+        success: true,
+        message: "Borrowed books summary retrieved successfully",
+        data: books
+    });
+});
