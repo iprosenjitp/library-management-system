a6d57367a38872ad11ceae5fdb99ffe8a9cb96c6
Prosenjit Pal <iprosenjitp@yahoo.com>
2025-06-20 17:47:50 +0600

    Update book model

--- src/app/models/book.model.ts
+++ src/app/models/book.model.ts
@@ -4,35 +4,44 @@ import { IBooks } from "../interfaces/book.interface";
 const bookSchema = new Schema<IBooks> ({
     title: {
         type: String,
-        required: true,
+        required: [true, "Book title is required"],
         trim: true
     },
     author: {
         type: String,
-        required: true,
+        required: [true, "Author is required"],
         trim: true
     },
     genre: {
         type: String,
-        enum: ['FICTION', 'NON_FICTION', 'SCIENCE', 'HISTORY', 'BIOGRAPHY', 'FANTASY'],
-        required: true
+        enum: {
+            values: ['FICTION', 'NON_FICTION', 'SCIENCE', 'HISTORY', 'BIOGRAPHY', 'FANTASY'],
+            message: `{VALUE}, is not a valid type`
+        },
+        required: [true, "Book genre is required"]
     },
     isbn: {
         type: String,
-        required: true
+        required: true,
+        unique: [true, "ISBN must be UNIQUE"],
+        trim: true
     },
     description: {
         type: String,
-        default: ""
+        default: "",
+        trim: true
     },
     copies: {
         type: Number,
-        required: true,
+        required: [true, "Book copies is required"],
     },
     available: {
         type: Boolean,
         default: true
     }
+}, {
+    versionKey: false,
+    timestamps: true
 });
 
 export const Book = model<IBooks>("Book", bookSchema);
