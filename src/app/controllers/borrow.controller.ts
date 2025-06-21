3d6d6b85886041c4fad97e63cca91bf048ed260e
Prosenjit Pal <iprosenjitp@yahoo.com>
2025-06-21 13:30:18 +0600

    Update aggregation logic in borrow

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -23,14 +23,49 @@ borrowRoutes.post('/', async (req: Request, res: Response) => {
     }
 });
 
-// GET - Retrieve all borrows
+// GET - Retrieve all borrows summary
 borrowRoutes.get('/', async (req: Request, res: Response) => {
-    const books = await Borrow.find();
+    try {
+        const borrowSummary = await Borrow.aggregate([
+            {
+                $group: {
+                    _id: "$book",
+                    totalQuantity: {$sum: "$quantity" }
+                }
+            }, {
+                $lookup: {
+                    from: "books",
+                    localField: "_id", 
+                    foreignField: "_id", 
+                    as: "bookDetails" 
+                }
+            }, {
+                $unwind: "$bookDetails" 
+            }, {
+                $project: {
+                    _id: 0,
+                    book : {
+                        title: "$bookDetails.title",
+                        isbn: "$bookDetails.isbn"
+                    },
+                    totalQuantity: 1,
+                }
+            }
+        ]);
+
     
-    res.status(201).json({
-        success: true,
-        message: "Borrowed books summary retrieved successfully",
-        data: books
-    });
+        res.status(201).json({
+            success: true,
+            message: "Borrowed books summary retrieved successfully",
+            data: borrowSummary
+        });
+
+    } catch (error: any) {
+        res.status(400).json({
+            success: false,
+            message: "Failed to retrieve borrows",
+            error: error instanceof Error ? error.message : error
+        });
+    }
 });
 
