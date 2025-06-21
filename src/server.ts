8b78fc3ca273703bab45c395a7c6b25e2f145ab8
Prosenjit Pal <iprosenjitp@yahoo.com>
2025-06-21 23:04:02 +0600

    Setup env

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import { Server } from 'http';
 import app from './app';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 let server: Server;
 const PORT = 5000;
 
 async function main() {
     try {
-        await mongoose.connect('mongodb+srv://mongodb:mongodb@cluster0.tlfvs3g.mongodb.net/library-ms-app?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect(process.env.DATABASE_URL as string);
 
         console.log("Connected to MongoDB using Mongoose");
         
